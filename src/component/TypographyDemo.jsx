6d67f3108d36f172b602b3e682c479fb1d496a64
Satyamray21 <satyamray43@gmail.com>
2025-04-09 09:33:09 +0530

    Typography concept done

---
+++ src/component/TypographyDemo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+function TypographyDemo() {
+  return (
+    <>
+    <Typography variant= "h1">Hello|Its Typography class</Typography>
+    <Typography variant= "h2">Hello|Its Typography class</Typography>
+    <Typography variant= "h3">Hello|Its Typography class</Typography>
+    <Typography variant= "h4">Hello|Its Typography class</Typography>
+    <Typography variant= "h5" sx = {{color:'red'}}>Hello|Its Typography class</Typography>
+    </>
+  )
+}
+
+export default TypographyDemo
